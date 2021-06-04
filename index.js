03d66719af12df84b02f20cc99d83150b7c8ab85
BillyMiles <williammiles201@gmail.com>
2021-06-04 08:48:36 -0400

    commit tree 17cc6338ffc69dfe21b9d0fd500caedadd60938d

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const mainer = document.getElementById('main');
+
+mainer.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+
+document.body.appendChild(newHeader)
+
+newHeader.innerHTML = "Billy is the champion";
+
